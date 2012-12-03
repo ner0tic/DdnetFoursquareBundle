c832dfad1f48aa19600527a31498fe0d87dc9bfc
David Durost <david.durost@gmail.com>
2012-12-03 12:31:06 -0500

    minor fixes

---
+++ DdnetFoursquareBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ddnet\FoursquareBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class DdnetFoursquareBundle extends Bundle
+{
+    /**
+     *      * {@inheritdoc}
+     *           */
+    public function build(ContainerBuilder $container)
+    {
+            parent::build($container);
+        }
+}
+>
